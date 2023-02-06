d8ae670f15084328428bb0488c037f41fe87622a
1v4n4 <ivananl@yahoo.com>
2023-02-06 12:56:17 +0100

    Add stream

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 
-import { readFile } from 'fs/promises';
+import fs from 'fs';
 import { parse } from "js2xmlparser";
-import { json } from "./helper.js";
+// import { json } from "./helper.js";
+import * as readline from 'node:readline';
 
-const xml = parse("supportForm", json);
-console.log(xml);
+
+const input =  fs.createReadStream(
+    new URL('../data/sample.jsonl', import.meta.url)
+)
+
+const output = fs.createWriteStream(
+  new URL('../data/output.xml', import.meta.url)
+);
+
+console.log("fileeee", input)
+
+const readFile = readline.createInterface({
+  input: input,
+  output: output,
+  terminal: false
+});
+
+const transform = line => {
+  console.log("linee", line);
+}
+
+readFile
+  .on('line', transform)
+  .on('close', function() {
+    console.log(`Created "${this.output.path}"`);
+  });
+
+
+//const xml = parse("supportForm", json);
+// console.log(xml);
